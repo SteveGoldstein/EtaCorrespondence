54e805c7c8aa6a84266bca2e809dcc8e795b154b
Steve Goldstein <sgoldstein@math.wisc.edu>
2018-03-26 04:37:18 -0500

    Saving XTables for GLn

---
+++ bin/saveXTables.m
@@ -0,0 +1,38 @@
+// magma -b n:=<N> p:=<P> file:=<XtableFile> bin/saveXTables.m
+
+//Simple script to save the character tables of GLn;
+
+load "../lib/save_group.m";
+
+SetColumns(0);
+SetAutoColumns(false);
+
+n := StringToInteger(n);
+p := StringToInteger(p);
+
+
+G := GL(n,p);
+X := CharacterTable(G);
+
+SaveGroup(G, file);
+
+quit;
+
+/*
+
+## do the feasible XTables for p=3,5,7;
+p=3; for n in {2..7}; do magma -b n:=$n p:=$p file:=~/MAGMA/CharacterTable/xtab_gl$n-${p}_af.db bin/saveXTables.m >& xtab_gl$n-$p.err & done
+
+p=5; for n in {2..5}; do magma -b n:=$n p:=$p file:=~/MAGMA/CharacterTable/xtab_gl$n-${p}_af.db bin/saveXTables.m >& xtab_gl$n-$p.err & done
+
+p=7; for n in {2..4}; do magma -b n:=$n p:=$p file:=~/MAGMA/CharacterTable/xtab_gl$n-${p}_af.db bin/saveXTables.m >& xtab_gl$n-$p.err & done
+
+## do the remaining feasible for n=2 and n=3
+n=2; for p in 11 13 17 19 23 29 31 37 41; do magma -b n:=$n p:=$p file:=~/MAGMA/CharacterTable/xtab_gl$n-${p}_af.db bin/saveXTables.m >& xtab_gl$n-$p.err & done
+
+n=3; for p in 11 13 17 19; do magma -b n:=$n p:=$p file:=~/MAGMA/CharacterTable/xtab_gl$n-${p}_af.db bin/saveXTables.m >& xtab_gl$n-$p.err & done
+
+*/
+
+
+
