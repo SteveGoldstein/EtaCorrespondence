07284654d00cd14791b00acc44b9bdee08ca769b
Steve Goldstein <sgoldstein@math.wisc.edu>
2019-05-06 16:31:24 -0500

    cranking out all tables

--- bin/organizeCharacterTable.m
+++ bin/organizeCharacterTable.m
@@ -13,13 +13,24 @@ GLn   := ReadGL(n,p);
 
 X := CharacterTable(GLn);
 TensorRank_n := ReadTensorRank(n,p);
-C<i> := ComplexField(3);
+C<i> := ComplexField(5);
+
+printf "rho,TensorRank";
+for j in [1..#X] do
+    printf ",ConjClass%o", j;
+end for;
+printf "\n";
 
 for i in [1..#X] do
     rnk := TensorRank_n[i];
 
     charRatios := [C!X[i,j]/X[i,1] : j in [1..#X]];
-    printf "%o, %o, %o\n",i, rnk, [charRatios[j]: j in [1..#X]] ;
+    printf "%o,%o", i, rnk;
+    for j in [1..#X] do
+	printf ",%o", Abs(charRatios[j]);
+    end for;
+    printf "\n";
+    //printf "%o, %o, %o\n",i, rnk, [Abs(charRatios[j]): j in [1..#X]] ;
     
 end for;
 quit;
---
+++ bin/organizeCharacterTable.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+nohup magma -b n:=2 p:=11 bin/organizeCharacterTable.m 2> XRatios/GL2-11.ratios.err |grep -v Loading > XRatios/GL2-11.ratios.csv & 
+nohup magma -b n:=2 p:=13 bin/organizeCharacterTable.m 2> XRatios/GL2-13.ratios.err |grep -v Loading > XRatios/GL2-13.ratios.csv & 
+nohup magma -b n:=2 p:=17 bin/organizeCharacterTable.m 2> XRatios/GL2-17.ratios.err |grep -v Loading > XRatios/GL2-17.ratios.csv & 
+nohup magma -b n:=2 p:=19 bin/organizeCharacterTable.m 2> XRatios/GL2-19.ratios.err |grep -v Loading > XRatios/GL2-19.ratios.csv & 
+nohup magma -b n:=2 p:=23 bin/organizeCharacterTable.m 2> XRatios/GL2-23.ratios.err |grep -v Loading > XRatios/GL2-23.ratios.csv & 
+nohup magma -b n:=2 p:=29 bin/organizeCharacterTable.m 2> XRatios/GL2-29.ratios.err |grep -v Loading > XRatios/GL2-29.ratios.csv & 
+nohup magma -b n:=2 p:=31 bin/organizeCharacterTable.m 2> XRatios/GL2-31.ratios.err |grep -v Loading > XRatios/GL2-31.ratios.csv & 
+nohup magma -b n:=2 p:=37 bin/organizeCharacterTable.m 2> XRatios/GL2-37.ratios.err |grep -v Loading > XRatios/GL2-37.ratios.csv & 
+nohup magma -b n:=2 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL2-3.ratios.err |grep -v Loading > XRatios/GL2-3.ratios.csv & 
+nohup magma -b n:=2 p:=41 bin/organizeCharacterTable.m 2> XRatios/GL2-41.ratios.err |grep -v Loading > XRatios/GL2-41.ratios.csv & 
+nohup magma -b n:=2 p:=5 bin/organizeCharacterTable.m 2> XRatios/GL2-5.ratios.err |grep -v Loading > XRatios/GL2-5.ratios.csv & 
+nohup magma -b n:=2 p:=7 bin/organizeCharacterTable.m 2> XRatios/GL2-7.ratios.err |grep -v Loading > XRatios/GL2-7.ratios.csv & 
+nohup magma -b n:=3 p:=11 bin/organizeCharacterTable.m 2> XRatios/GL3-11.ratios.err |grep -v Loading > XRatios/GL3-11.ratios.csv & 
+nohup magma -b n:=3 p:=13 bin/organizeCharacterTable.m 2> XRatios/GL3-13.ratios.err |grep -v Loading > XRatios/GL3-13.ratios.csv & 
+nohup magma -b n:=3 p:=17 bin/organizeCharacterTable.m 2> XRatios/GL3-17.ratios.err |grep -v Loading > XRatios/GL3-17.ratios.csv & 
+nohup magma -b n:=3 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL3-3.ratios.err |grep -v Loading > XRatios/GL3-3.ratios.csv & 
+nohup magma -b n:=3 p:=5 bin/organizeCharacterTable.m 2> XRatios/GL3-5.ratios.err |grep -v Loading > XRatios/GL3-5.ratios.csv & 
+nohup magma -b n:=3 p:=7 bin/organizeCharacterTable.m 2> XRatios/GL3-7.ratios.err |grep -v Loading > XRatios/GL3-7.ratios.csv & 
+nohup magma -b n:=4 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL4-3.ratios.err |grep -v Loading > XRatios/GL4-3.ratios.csv & 
+nohup magma -b n:=4 p:=5 bin/organizeCharacterTable.m 2> XRatios/GL4-5.ratios.err |grep -v Loading > XRatios/GL4-5.ratios.csv & 
+nohup magma -b n:=4 p:=7 bin/organizeCharacterTable.m 2> XRatios/GL4-7.ratios.err |grep -v Loading > XRatios/GL4-7.ratios.csv & 
+nohup magma -b n:=5 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL5-3.ratios.err |grep -v Loading > XRatios/GL5-3.ratios.csv & 
+nohup magma -b n:=5 p:=5 bin/organizeCharacterTable.m 2> XRatios/GL5-5.ratios.err |grep -v Loading > XRatios/GL5-5.ratios.csv & 
+nohup magma -b n:=6 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL6-3.ratios.err |grep -v Loading > XRatios/GL6-3.ratios.csv & 
+nohup magma -b n:=7 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL7-3.ratios.err |grep -v Loading > XRatios/GL7-3.ratios.csv & 
+nohup magma -b n:=8 p:=3 bin/organizeCharacterTable.m 2> XRatios/GL8-3.ratios.err |grep -v Loading > XRatios/GL8-3.ratios.csv & 
